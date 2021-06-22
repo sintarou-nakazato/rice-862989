3b92a212632351af55bd91010615215a705fe8ba
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-22 20:21:55 +0900

    foodテストコード

---
+++ spec/factories/foods.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :food do
+    title                  { 'ご飯' }
+    catch_copy             {'こんにちは'}
+    menu                   { '卵3個' }
+    association :user
+
+    after(:build) do |food|
+      food.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
-
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @food = FactoryBot.build(:food)
+  end
+
+  describe 'ユーザー投稿機能' do
+    context '内容に問題がない場合' do
+      it 'すべての項目の入力が存在すれば登録できる' do
+        expect(@food).to be_valid
+      end
+    end
+    context '内容に問題がある場合' do
+      it 'titleが空では登録できない' do
+        @food.title = ''
+        @food.valid?
+        expect(@food.errors.full_messages).to include "タイトルを入力してください"
+      end
+      it 'catch_copyが空なら登録できない' do
+        @food.catch_copy = ''
+        @food.valid?
+        expect(@food.errors.full_messages).to include "キャッチコピーを入力してください"
+      end
+      it 'menuが空なら登録できない' do
+        @food.menu = ''
+        @food.valid?
+        expect(@food.errors.full_messages).to include "レシピを入力してください"
+      end
+      it 'image何からなら登録できない' do
+        @food.image = nil
+        @food.valid?
+        expect(@food.errors.full_messages).to include "画像を入力してください"
+      end
+    end
+  end
 end
