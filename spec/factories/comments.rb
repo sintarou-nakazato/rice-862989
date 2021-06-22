dec96fdcfe7abc40beac5d8e970dc042816351f6
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-22 20:33:12 +0900

    commentテストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    text    {}
+    text    { 'こんにちは' }
+    association :user
+    association :food
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
+RSpec.describe Food, type: :model do
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザーコメント機能' do
+    context '内容に問題がない場合' do
+      it 'すべての項目の入力が存在すれば登録できる' do
+        expect(@comment).to be_valid
+      end
+    end
+    context '内容に問題がある場合' do
+      it 'textが空では登録できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "Textを入力してください"
+      end
+    end
+  end
 end
